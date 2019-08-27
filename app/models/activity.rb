5d45e8d998faa5af90d030160e949eed0a9009e4
isabelle Leclaire <isabelle.leclaire@gmail.com>
2019-08-27 11:13:23 +0200

    mountuploader dans models activity et user

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :place
   belongs_to :subscription
   has_many :act_bookings
+  mount_uploader :photo_one, PhotoUploader
+  mount_uploader :photo_two, PhotoUploader
+  mount_uploader :photo_three, PhotoUploader
 end
