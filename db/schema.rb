d651b5b8104dc09bf1d5a8c599534dab25bcf54a
Maxime Fulconis <maximefulconis@gmail.com>
2019-08-27 16:51:26 +0200

    index and map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_121727) do
+ActiveRecord::Schema.define(version: 2019_08_27_134202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2019_08_27_121727) do
     t.string "access"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reviews", force: :cascade do |t|
