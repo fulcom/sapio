d15e81c022c4a9a1fe8d6e4552c83d483b77e1a1
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-27 15:58:19 +0200

    vue new pour subs_booking

--- app/controllers/subs_bookings_controller.rb
+++ app/controllers/subs_bookings_controller.rb
@@ -1,7 +1,7 @@
 class SubsBookingsController < ApplicationController
   # before_action :set_subs_booking, only: [:show]
   def index
-    # raise
+    @subs_bookings = current_user.subs_bookings.order(end_date: :desc)
   end
 
   def show
@@ -9,14 +9,19 @@ class SubsBookingsController < ApplicationController
   end
 
   def new
-    @subs_booking = Subs_booking.new
+    @subs_booking = SubsBooking.new
+    @subscriptions = Subscription.all
   end
 
   def create
-    @subs_booking = Subs_booking.new(subs_booking_params)
-    @subs_booking.user = current_user
-    if @subs_booking.save
-      redirect_to subs_booking_path(@subs_booking)
+    subscription = Subscription.find(params[:subs_booking][:subscription_id])
+    subs_booking = SubsBooking.new(subscription_id: subscription.id)
+    subs_booking.user = current_user
+    subs_booking.start_date = Date.today
+    subs_booking.end_date = Date.today + 1.month
+
+    if subs_booking.save
+      redirect_to subs_bookings_path
     else
       render :new
     end
@@ -24,11 +29,11 @@ class SubsBookingsController < ApplicationController
 
   private
 
-  def set_subs_booking
-    @subs_booking = Subs_booking.find(params[:id])
-  end
+  # def set_subs_booking
+  #   @subs_booking = Subs_booking.find(params[:id])
+  # end
 
-  def subs_booking_params
-    params.require(:subs_booking).permit(:start_date, :end_date, :subscription_id, :user_id)
-  end
+  # def subs_booking_params
+  #   params.require(:subs_booking).permit(:subscription_id)
+  # end
 end
