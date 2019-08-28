17f03ab3a649f5f5c06900b1264cfe05083e34d0
isabelle Leclaire <isabelle.leclaire@gmail.com>
2019-08-28 14:21:19 +0200

    ajout colonne country et update seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_134202) do
+ActiveRecord::Schema.define(version: 2019_08_28_121835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_134202) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "country"
   end
 
   create_table "reviews", force: :cascade do |t|
