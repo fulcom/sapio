9d1a7e2afbb826a90dc236012d35094c3421e06e
Charles Dieulefet <charles.dieulefet@gmail.com>
2019-08-28 12:14:56 +0200

    in progress

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
 import "bootstrap";
+import "../plugins/flatpickr"
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
-import "../plugins/flatpickr"
+
 import { initStarRating } from '../plugins/init_star_rating';
 
 initStarRating();
