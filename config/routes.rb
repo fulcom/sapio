48877df25ba5f89874114f9ff11c4784c05582c2
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-30 11:46:41 +0200

    route users/profile + controller User + def et view profile

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   # resources :places, only: [:show]
   resources :subscriptions, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "users/profile", to: "user#profile"
 end
