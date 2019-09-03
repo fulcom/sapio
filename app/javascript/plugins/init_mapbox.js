5059804dd6ff0b70b60783dd4abccc97c31e8b14
isabelle Leclaire <isabelle.leclaire@gmail.com>
2019-09-03 14:56:03 +0200

    map zoom scroll disable

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -37,6 +37,7 @@ const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.scrollZoom.disable();
 };
 
 const initMapbox = () => {
@@ -47,7 +48,7 @@ const initMapbox = () => {
         enableHighAccuracy: true
       },
       fitBoundsOptions: {
-        maxZoom: 12
+        maxZoom: 11
       },
       trackUserLocation: true
     }));
